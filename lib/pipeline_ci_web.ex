55a513d7e18bd83121045f061ba8a265bc3867b7
unknown <stevancccampos@gmail.com>
2023-03-09 13:30:50 -0300

    another deploy

--- lib/pipeline_ci_web.ex
+++ lib/pipeline_ci_web.ex
@@ -91,6 +91,7 @@ defmodule PipelineCiWeb do
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
+      import PipelineCiWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
